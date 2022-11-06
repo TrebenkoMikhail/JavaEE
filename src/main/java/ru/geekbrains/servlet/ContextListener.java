c1fe46e2e93d67f04de786cd70d79b81781b0b7f
mihatrebenko <mihatrebenko@mail.ru>
2022-11-06 18:16:15 +0500

    init com

---
+++ src/main/java/ru/geekbrains/servlet/ContextListener.java
@@ -0,0 +1,49 @@
+package ru.geekbrains.servlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+@WebListener
+public class ContextListener implements ServletContextListener {
+
+    private Logger logger = LoggerFactory.getLogger(ContextListener.class);
+
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext context = sce.getServletContext();
+        logger.info("Context initialization: " +
+                context.getContextPath());
+        String jdbcConnectionString =
+                context.getInitParameter("jdbcConnectionString");
+        String username = context.getInitParameter("dbUsername");
+        String password = context.getInitParameter("dbPassword");
+        if (jdbcConnectionString != null || username != null) {
+            logger.error("Connection string and DB username must be specified");
+            return;
+        }
+        try {
+            Connection connection = DriverManager.getConnection(jdbcConnectionString, username, password);
+            context.setAttribute("jdbcConnection", connection);
+        } catch (SQLException e) {
+            logger.error("", e);
+        }
+    }
+
+    public void contextDestroyed(ServletContextEvent sce) {
+        ServletContext context = sce.getServletContext();
+        Connection conn = (Connection) context.getAttribute("jdbcConnection");
+        if (conn == null) {
+            return;
+        }
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            logger.error("", e);
+        }
+    }
+
+}
---
+++ src/main/java/ru/geekbrains/servlet/Hometask1.java
@@ -0,0 +1,44 @@
+package ru.geekbrains.servlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+
+
+@WebServlet(name = "JdbcServlet", urlPatterns = "/jdbc_servlet")
+public class Hometask1 extends HttpServlet {
+    private Logger logger = LoggerFactory.getLogger(ServletRequestListener.class);
+    private Connection conn;
+
+    @Override
+    public void init() throws ServletException {
+        ServletContext context = getServletContext();
+        conn = (Connection) context.getAttribute("jdbcConnection");
+        if (conn == null) {
+            throw new ServletException("No JDBC connection in Servlet Context");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse
+            resp) throws ServletException, IOException {
+        logger.info("Get all tables");
+            try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SHOW TABLES;");
+            while (rs.next()) {
+                String tableName = rs.getString(0);
+                resp.getWriter().println("<p> " + Product.class + "</p>");
+            }
+        } catch (SQLException e) {
+            throw new ServletException(e);
+        }
+    }
+}
