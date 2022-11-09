db0be4a026bda2fc38dcd4883f8cabfe86b28d8a
mihatrebenko <mihatrebenko@mail.ru>
2022-11-10 03:15:03 +0500

    Было много работы, просмотрел вебинар вчера, сегодня прочитал методичку и сразу за дз. Наделал кучу ошибок, но завтра опять весь день на работе. Не успею доделать нормально. Поэтому сдаю как есть. Извините. В субботу обещаю всё исправить.

---
+++ src/main/java/Lesson2/AppConfige.java
@@ -0,0 +1,27 @@
+package Lesson2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    products.getProducts();
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.FIELD)
+    @Scope
+    public @interface cart {
+        @Override
+        ProductRepository products = products.equals(1).remove;
+        ProductRepository products = products.getClass();
+    }
+
+
+}
---
+++ src/main/java/Lesson2/HW2.java
@@ -0,0 +1,11 @@
+package Lesson2;
+
+
+public class HW2 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
